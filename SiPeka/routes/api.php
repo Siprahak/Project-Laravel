fe98f81cd35c37601004255da860dffe4596cfa3
Zhillan <zhillan.arrafi@gmail.com>
2025-06-06 10:12:52 +0700

    merapihkan authorization untuk persiapan inisiasi react

---
+++ SiPeka/routes/api.php
@@ -0,0 +1,53 @@
+<?php
+
+use App\Http\Controllers\AdminController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ComplaintController;
+use App\Http\Controllers\AttachmentController;
+use App\Http\Controllers\RatingController;
+use App\Http\Controllers\ResponseController;
+use App\Http\Controllers\Api\AuthController;
+
+
+// Rute pendaftaran dan login pengguna
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/me', [AuthController::class, 'me']);
+    Route::apiResource('complaints', ComplaintController::class)->only(['store', 'show', 'destroy']);
+    Route::apiResource('attachments', AttachmentController::class)->only(['store', 'show', 'destroy']);
+    Route::apiResource('ratings', RatingController::class)->only(['store', 'show', 'destroy']);
+
+});
+
+// Rute hanya untuk admin
+Route::middleware(['auth:sanctum', 'cekrole:admin'])->group(function () {
+    Route::apiResource('categories', CategoryController::class)->only(['index', 'store', 'show', 'update', 'destroy']);
+    Route::apiResource('responses', ResponseController::class)->only(['index', 'store', 'show', 'update', 'destroy']);
+    Route::apiResource('users', UserController::class)->only(['index', 'store', 'show','update', 'destroy']);
+    Route::apiResource('complaints', ComplaintController::class)->only(['index', 'store', 'show','update', 'destroy']);
+    Route::apiResource('attachments', AttachmentController::class)->only(['index', 'store', 'show','update', 'destroy']);
+    Route::apiResource('ratings', RatingController::class)->only(['index', 'store', 'show','update', 'destroy']);
+    
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+
+});
+
+
+// Route::middleware('auth:sanctum', 'cekrole:user,admin')->group(function () {
+//     Route::apiResource('complaints', ComplaintController::class)->only(['index', 'store', 'show', 'destroy']);
+//     Route::apiResource('attachments', AttachmentController::class)->only(['index', 'store', 'show', 'destroy']);
+//     Route::apiResource('ratings', RatingController::class)->only(['index', 'store', 'show', 'destroy']);
+// });
+
+// Rute untuk admin
+// Route::middleware('auth:sanctum', 'cekrole:admin')->group(function () {
+    
+// });
