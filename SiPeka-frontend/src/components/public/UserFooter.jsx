ba2af6f514b336deecf52d0c5223ebe6ea9bd3fc
anasikhzf <anasikhzf@gmail.com>
2025-06-12 16:45:14 +0700

    change the navbar to be responsive

--- SiPeka-frontend/src/components/public/UserFooter.jsx
+++ SiPeka-frontend/src/components/public/UserFooter.jsx
@@ -6,15 +6,6 @@ export default function UserFooter() {
 
   return (
     <footer className="bg-[#141414] py-6 px-4 mt-auto">
-      <div className="flex justify-center mb-4">
-        <img src="/src/assets/logo.png" alt="Logo" className="h-8" />
-      </div>
-      <div className="flex justify-center gap-8 mb-4">
-        <NavLink to="/user/home" className={linkClass}>Home</NavLink>
-        <NavLink to="/user/complaints" className={linkClass}>Complaints</NavLink>
-        <NavLink to="/user/ratings" className={linkClass}>Ratings</NavLink>
-        <NavLink to="/user/profile" className={linkClass}>Profile</NavLink>
-      </div>
       <p className="text-[#a9a9a9] text-sm border-t pt-2 pl-5" style={{ borderTopColor: "#373737" }}>2025. SiPeka All Rights Reserved</p>
     </footer>
   );
